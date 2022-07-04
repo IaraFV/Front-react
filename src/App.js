145adb183ae940b0d0a2ef0ceb74a883183ede46
Iara Ferreira <iaraferrevit2@gmail.com>
2022-07-04 14:23:03 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from './components/Home'
 import Pessoas from './components/Pessoas'
 import Equipes from './components/Equipes'
-import { BrowserRouter, Switch, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import {Nav} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Activity, Calendar2Check, CardChecklist, Check, ListTask, People, Projector } from 'react-bootstrap-icons';
@@ -13,7 +13,7 @@ function App() {
 
  <div className='App' >
     <BrowserRouter >
-    <Nav variant='tabs' style={{background: '#171821', height: '80px'}}>
+    <Nav variant='tabs' style={{background: '#171821', height: '80px', border:"none"}}>
       <Nav.Link style={{background: '#171821', color: '#87888C', marginLeft:'2%', fontSize:'1.3rem'}} as={Link} to="/">Home</Nav.Link>
       <Nav.Link style={{background: '#171821', color: '#87888C', marginLeft:'2%', fontSize:'1.3rem'}} as={Link} to="/Pessoas">Pessoas</Nav.Link>
       <Nav.Link style={{background: '#171821', color: '#87888C', marginLeft:'2%', fontSize:'1.3rem'}} as={Link} to="/Equipes">Equipes</Nav.Link>
@@ -22,6 +22,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/Pessoas" element={<Pessoas/>}></Route>
+      <Route path="/Equipes" element={<Equipes/>}></Route>
     </Routes>
     </BrowserRouter>
 </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,12 @@ import './Home.css'
 import teste from './img/undraw_Code_typing_re_p8b9-removebg-preview.png'
 import { Activity, Calendar2Check, CardChecklist, Check, ListTask, People, Projector } from 'react-bootstrap-icons';
 import ProgressBar from 'react-bootstrap/ProgressBar'
+import Card from 'react-bootstrap/Card'
+import { Graf } from "./graf";
+import { Graf2 } from "./graf2";
 function Home (){
     return(
-        <body style={{background: '#171821'}}>
+        <>
             <div id="imginicio" ><img src={teste} alt=" " width={'10%'} /></div>
             <div id="container">
                 <h1 id="h1" style={{color: '#FFF'}} >Olá</h1>
@@ -15,7 +18,7 @@ function Home (){
             <div id="geral">
 
                 <div id="cards" style={{background: '#21222D'}}>
-                    <div id="pritext"><h1>Titulo</h1><p>Porcentagem</p></div>
+                    <div id="pritext"><h1>Titulo</h1><h5>Porcentagem</h5></div>
                     
                     <div id="cardum"  style={{background: '#171821'}}><People id="iconpeople" color="#FEB95A" size={56}  />
                     <h2>300</h2>
@@ -36,21 +39,103 @@ function Home (){
                 </div>
 
                 <div id="tabela" style={{background: '#21222D'}}>
-
+                   <h1>Task's adicionadas</h1>
+                   <p>Task's adicionadas em ua semana</p>
+                    <Graf2/>
                 </div>
             </div>
 
             <div id="geraltwo">
                 <div id="progress" style={{background: '#21222D'}}>
-                    
+                    <h1>Task's</h1>
+                    <h5>Concluídas</h5>
+                    <div id="concluidas">
+                        <div id="taskum" style={{background: '#171821', borderRadius:'2rem'}}>
+                            <Card  style={{ width: '18rem', background:'#171821', border:'1px solid #A9DFD8', borderRadius:'2rem'}}>
+                                <Card.Header  style={{color:"#A9DFD8"}} >Concluida</Card.Header>
+                                <Card.Body>
+                                <Card.Title style={{color:"#fff"}}>Título da task</Card.Title>
+                                <Card.Text style={{color:"#A0A0A0"}}>
+                                    Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.
+                                </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div id="taskdois" style={{background: '#171821', borderRadius:'2rem'}}>
+                             <Card  style={{ width: '18rem', background:'#171821', border:'1px solid #A9DFD8', borderRadius:'2rem'}}>
+                                <Card.Header  style={{color:"#A9DFD8"}} >Concluida</Card.Header>
+                                <Card.Body>
+                                <Card.Title style={{color:"#fff"}}>Título da task</Card.Title>
+                                <Card.Text style={{color:"#A0A0A0"}}>
+                                    Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.
+                                </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div id="tasktres" style={{background: '#171821', borderRadius:'2rem'}}>
+                            <Card  style={{ width: '18rem', background:'#171821', border:'1px solid #A9DFD8', borderRadius:'2rem'}}>
+                                <Card.Header  style={{color:"#A9DFD8"}} >Concluida</Card.Header>
+                                <Card.Body>
+                                <Card.Title style={{color:"#fff"}}>Título da task</Card.Title>
+                                <Card.Text style={{color:"#A0A0A0"}}>
+                                    Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.
+                                </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
+
+                    <div id="concluidas">
+                        <div id="taskum" style={{background: '#171821', borderRadius:'2rem'}}>
+                            <Card  style={{ width: '18rem', background:'#171821', border:'1px solid #A9DFD8', borderRadius:'2rem'}}>
+                                <Card.Header  style={{color:"#A9DFD8"}} >Concluida</Card.Header>
+                                <Card.Body>
+                                <Card.Title style={{color:"#fff"}}>Título da task</Card.Title>
+                                <Card.Text style={{color:"#A0A0A0"}}>
+                                    Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.
+                                </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div id="taskdois" style={{background: '#171821', borderRadius:'2rem'}}>
+                             <Card  style={{ width: '18rem', background:'#171821', border:'1px solid #A9DFD8', borderRadius:'2rem'}}>
+                                <Card.Header  style={{color:"#A9DFD8"}} >Concluida</Card.Header>
+                                <Card.Body>
+                                <Card.Title style={{color:"#fff"}}>Título da task</Card.Title>
+                                <Card.Text style={{color:"#A0A0A0"}}>
+                                    Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.
+                                </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div id="tasktres" style={{background: '#171821', borderRadius:'2rem'}}>
+                            <Card  style={{ width: '18rem', background:'#171821', border:'1px solid #A9DFD8', borderRadius:'2rem'}}>
+                                <Card.Header  style={{color:"#A9DFD8"}} >Concluida</Card.Header>
+                                <Card.Body>
+                                <Card.Title style={{color:"#fff"}}>Título da task</Card.Title>
+                                <Card.Text style={{color:"#A0A0A0"}}>
+                                    Some quick example text to build on the card title and make up the bulk
+                                    of the card's content.
+                                </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
                 </div>
 
-                <div id="task" style={{background: '#21222D'}}></div>
+                <div id="pc" style={{background: '#21222D'}}>
+                <h2>Pesssoas Adicionadas</h2>
+                <h5>Pessoas adicionadas no período de uma semana</h5>
+                <Graf/>
+                </div>
             </div>
-                
-      
-        
-        </body>
+                       
+      </>
     
 );
 }
