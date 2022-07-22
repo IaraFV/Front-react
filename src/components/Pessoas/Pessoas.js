5910f521d6d45ba88382c126ac292a3aa390a39e
Iara Ferreira <iaraferrevit2@gmail.com>
2022-07-22 07:38:29 -0300

    Post deu certo

--- src/components/Pessoas/Pessoas.js
+++ src/components/Pessoas/Pessoas.js
@@ -96,6 +96,4 @@ return(
 )
 }
 
-            
-    
 export default Pessoas;
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -6,11 +6,12 @@ import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup'
 import './Post.css'
-
+import { useHistory } from 'react-router-dom'
+import axios from "axios";
 
 const validacaoPost = yup.object().shape({
-    nome_pessoa:  yup.string().required("O nome é obrigatorio!").max(40),
-    funcao_pessoa: yup.string().required("A função é obrigatoria").max(40),
+    nome_pessoa:  yup.string().required("O nome é obrigatorio!"),
+    funcao_pessoa: yup.string().required("A função é obrigatoria")
 })
 
 function Post() {
@@ -19,7 +20,14 @@ function Post() {
     resolver: yupResolver(validacaoPost)
 })
 
-    const addPost = data => console.log(data)
+    const addPost = data => axios.post("https://sistema-aprendizes-brisanet-go.herokuapp.com/pessoas/", data)
+    .then(() => {
+        console.log("foi")
+    })
+    .catch(() => {
+        console.log("n foi")
+    })
+
     return(
 <>
         <div>
@@ -36,14 +44,16 @@ function Post() {
                     <div className="body-post">
 
                         <form onSubmit={handleSubmit(addPost)}>
+
                             <div className="fields">
-                                <label>Titulo</label>
-                                <input type="text" name="nome_pessoa" {...register("nome")}/>
+                                <label>Nome</label>
+                                <input type="text" name="nome_pessoa" {...register("nome_pessoa")}/>
                                 <p className="error-message">{errors.nome_pessoa?.message} </p>
                             </div>
+
                             <div className="fields">
-                                <label>Titulo</label>
-                                <input type="text" name="funcao" {...register("funcao_pessoa")}/>
+                                <label>Função</label>
+                                <input type="text" name="funcao_pessoa" {...register("funcao_pessoa")}/>
                                 <p className="error-message">{errors.funcao_pessoa?.message} </p>
                             </div>
 
