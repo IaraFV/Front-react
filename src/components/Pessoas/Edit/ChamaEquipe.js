763f440d92dc51a5ce7b33ec6f76bc3c0c71c973
Iara <lucasmartinsnara@gmail.com>
2022-07-26 09:18:27 -0300

    up

--- src/components/Pessoas/Edit/ChamaEquipe.js
+++ src/components/Pessoas/Edit/ChamaEquipe.js
@@ -1,10 +1,11 @@
-import * as React from 'react';
+import React, { useState, useEffect } from "react";
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-
+import { useParams } from 'react-router-dom'
+import axios from "axios";
 
 export default function ChamaEquipe() {
     const [age, setAge] = React.useState('');
@@ -12,6 +13,33 @@ export default function ChamaEquipe() {
     const handleChange = (event) => {
       setAge(event.target.value);
     };
+    const [initialequipe, setInitialequipe] = useState([])
+    const [equipe, setequipe] = useState([])
+
+    useEffect(() => {
+        const fetchequipe = async () => {
+            try {
+                const response = await fetch('https://sistema-aprendizes-brisanet-go.herokuapp.com/equipes/');
+                const data = await response.json();
+                setInitialequipe(data);
+                setequipe(data);
+
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchequipe();
+    }, []);
+    const handlechange = ({target}) =>{
+        if(!target.value) {
+            setequipe(initialequipe)
+            return
+        }
+        const filterepo = equipe.filter(({id_equipe}) =>
+        id_equipe.includes(target.value))
+
+        setequipe(filterepo);
+    }
   
     return (
       <Box sx={{ minWidth: 120 }}>
@@ -19,16 +47,20 @@ export default function ChamaEquipe() {
           <InputLabel id="demo-simple-select-label">Age</InputLabel>
           <Select
             labelId="demo-simple-select-label"
-            id="demo-simple-select"
+            id_equipe="demo-simple-select"
             value={age}
             label="Age"
-            onChange={handleChange}
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
+            onChange={handleChange}>
+                { equipe.map((repo) =>
+            <MenuItem value={10} key={equipe.id_equipe}>{repo.id_equipe}</MenuItem>
+            )}
           </Select>
         </FormControl>
       </Box>
     );
   }
+  /**
+   * const getequipe = axios.create({
+        baseURL:'https://sistema-aprendizes-brisanet-go.herokuapp.com/equipes/'
+    });
+  */
