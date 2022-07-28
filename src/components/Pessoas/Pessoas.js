75675ac9cd90b256cf05ca00965f5c1fce906ace
iara <iaraferrevit2@gmail.com>
2022-07-28 11:49:49 -0300

    Filtro feito finalmente

--- src/components/Pessoas/Pessoas.js
+++ src/components/Pessoas/Pessoas.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import './Pessoas.css';
-import axios  from "axios";
+import axios from "axios";
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
@@ -12,15 +12,19 @@ import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Link } from 'react-router-dom';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import Filter from "./filtro";
+
 function Pessoas() {
 
-    const [ posts, setPosts ] = useState([])
-    
+    const [posts, setPosts, setInitialPosts, initialPosts] = useState([])
+   
+   
 
     useEffect(() => {
-            axios.get('https://sistema-aprendizes-brisanet-go.herokuapp.com/pessoas/')
+        axios.get('https://sistema-aprendizes-brisanet-go.herokuapp.com/pessoas/')
             .then((response) => {
                 setPosts(response.data)
+                setInitialPosts(response.data);
             })
             .catch(() => {
                 console.log("deu errado")
@@ -28,82 +32,97 @@ function Pessoas() {
     }, []
     )
 
+    const handlechange = ({ target }) => {
+        if (!target.value) {
+            setPosts(initialPosts)
+            return
+        }
+        const filterepo = posts.filter(({ nome_pessoa }) =>
+            nome_pessoa.includes(target.value))
 
-    function deletePost (id_pessoa) {
+        setPosts(filterepo);
+    }
+
+    function deletePost(id_pessoa) {
         axios.delete(`https://sistema-aprendizes-brisanet-go.herokuapp.com/pessoas/${id_pessoa}`)
         setPosts(posts.filter(post => post.id_pessoa !== id_pessoa))
     }
 
-return(
-      <div >
-         <div> 
-            <div id="informativo">
-                <h1 id="titlepessoa" style={{color: '#fff', marginLeft: '5%', marginTop: '4%'}}>Cadastro de Pessoas</h1>
-                <div id="usuarios">
-                <Avatar sx={{ bgcolor: [500] }} aria-label="recipe">
-                    i
-                </Avatar>
-                <Avatar sx={{ bgcolor: [500] }} aria-label="recipe">
-                    f
-                </Avatar>
-                <Avatar sx={{ bgcolor: [500] }} aria-label="recipe">
-                    v
-                </Avatar>
+    return (
+        <div >
+            <div>
+                <div id="informativo">
+                    <h1 id="titlepessoa" style={{ color: '#fff', marginLeft: '5%', marginTop: '4%' }}>Cadastro de Pessoas</h1>
+                    <div id="usuarios">
+                        <Avatar sx={{ bgcolor: [500] }} aria-label="recipe">
+                            i
+                        </Avatar>
+                        <Avatar sx={{ bgcolor: [500] }} aria-label="recipe">
+                            f
+                        </Avatar>
+                        <Avatar sx={{ bgcolor: [500] }} aria-label="recipe">
+                            v
+                        </Avatar>
+                    </div>
+                    <Link to="/Post">
+                        <button className="btn-adicionar">Adicionar Pessoa</button>
+                    </Link>
+                    <div>
+                        <div>
+                            <input type={"text"} onChange={handlechange}></input>
+                        </div>
+                    </div>
+
+                    <p style={{ color: '#fff', display: 'flex', justifyContent: 'flex-end', marginTop: '-2%', marginRight: '1%' }}></p>
                 </div>
-                <Link to="/Post">
-                    <button className="btn-adicionar">Adicionar Pessoa</button>
-                </Link>
-                
-                <p style={{color: '#fff', display: 'flex', justifyContent: 'flex-end', marginTop: '-2%', marginRight: '1%'}}></p>
-            </div>
-            <div id="geralpessoas">
-         {
-            posts.map((posts,key) => {
+                <div id="geralpessoas">
+                    {
+                        posts.map((posts, key) => {
 
-            return (
-            <div className="cardpessoas">
-            <Card container spacing={2}  sx={{ width: 345, bgcolor: '#21222D', color: 'white' }} key={key} >
-                <CardHeader className="titlecinco"
-                avatar={<Avatar sx={{ bgcolor: [500] }} aria-label="recipe">i</Avatar>}
+                            return (
+                                <div className="cardpessoas">
+                                    <Card container spacing={2} sx={{ width: 345, bgcolor: '#21222D', color: 'white' }} key={key} >
+                                        <CardHeader className="titlecinco"
+                                            avatar={<Avatar sx={{ bgcolor: [500] }} aria-label="recipe">i</Avatar>}
 
-                action={
-                    <IconButton aria-label="settings" >
-                    <Link to={{ pathname: `/Inspecionar/${posts.id_pessoa}` }}><VisibilityIcon  
-                    sx={{color: '#FFFFFF', marginTop: '-108%'}}/></Link>
-                    </IconButton>
-                }
-                
-                title={
-                posts.nome_pessoa
-                }
-                />
-            
-                <CardContent sx={{color: 'white' }}>
-                <Typography variant="body2" color="white">
-                {posts.funcao_pessoa}
-                </Typography>
-                </CardContent>
-                
-                <CardActions disableSpacing>
-                <IconButton> 
-                     <Link to={{ pathname: `/Edit/${posts.id_pessoa}` }}>
-                        <ModeEditIcon sx={{color: '#E9C46A'}}/>
-                     </Link>
-                </IconButton>
+                                            action={
+                                                <IconButton aria-label="settings" >
+                                                    <Link to={{ pathname: `/Inspecionar/${posts.id_pessoa}` }}><VisibilityIcon
+                                                        sx={{ color: '#FFFFFF', marginTop: '-108%' }} /></Link>
+                                                </IconButton>
+                                            }
 
-                <IconButton onClick={() => deletePost(posts.id_pessoa) } aria-label="share" sx={{color: '#E9C46A'}} >
-                    <DeleteIcon  />
-                </IconButton>
-                </CardActions>
-            </Card>
+                                            title={
+                                                posts.nome_pessoa
+                                            }
+                                        />
+
+                                        <CardContent sx={{ color: 'white' }}>
+                                            <Typography variant="body2" color="white">
+                                                {posts.funcao_pessoa}
+                                            </Typography>
+                                        </CardContent>
+
+                                        <CardActions disableSpacing>
+                                            <IconButton>
+                                                <Link to={{ pathname: `/Edit/${posts.id_pessoa}` }}>
+                                                    <ModeEditIcon sx={{ color: '#E9C46A' }} />
+                                                </Link>
+                                            </IconButton>
+
+                                            <IconButton onClick={() => deletePost(posts.id_pessoa)} aria-label="share" sx={{ color: '#E9C46A' }} >
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </CardActions>
+                                    </Card>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
-                )
-              })
-            }
-            </div>
-</div>
         </div>
-)
+    )
 }
 
 export default Pessoas;
---
+++ src/components/Pessoas/filtro.js
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from "react";
+
+function Filter (){
+    const [initialpessoa, setInitialpessoa] = useState([])
+    const [pessoa, setPessoa] = useState([])
+
+    useEffect(() => {
+        const fetchpessoa = async () => {
+            try {
+                const response = await fetch('https://sistema-aprendizes-brisanet-go.herokuapp.com/pessoas/');
+                const data = await response.json();
+                setInitialpessoa(data);
+                setPessoa(data);
+
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchpessoa();
+    }, []);
+    const handlechange = ({target}) =>{
+        if(!target.value) {
+            setPessoa(initialpessoa)
+            return
+        }
+        const filterepo = pessoa.filter(({nome_pessoa}) =>
+        nome_pessoa.includes(target.value))
+
+        setPessoa(filterepo);
+    }
+
+    return(
+        <div>
+            <div>
+                <input type={"text"} onChange={handlechange}></input>
+            </div>
+            <div>
+            <ul>
+                {pessoa.map((repo) =>
+                <li key={pessoa.id_pessoa}>{repo.nome_pessoa} </li>
+                )}
+            </ul>
+            </div>
+        </div>
+);
+}
+export default Filter;
