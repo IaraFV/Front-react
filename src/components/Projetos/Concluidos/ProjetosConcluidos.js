5acb5a26ea17bdd76612c1986717f5a8a2ef4dd1
iara <iaraferrevit2@gmail.com>
2022-08-01 11:57:42 -0300

    projeto

--- src/components/Projetos/Concluidos/ProjetosConcluidos.js
+++ src/components/Projetos/Concluidos/ProjetosConcluidos.js
@@ -12,16 +12,18 @@ import SpeedDialAction from '@mui/material/SpeedDialAction';
 import { BsAlarm } from "react-icons/bs";
 import { BsCheckSquare } from "react-icons/bs";
 import { BsHourglassSplit } from "react-icons/bs";
+import Avatar from '@mui/material/Avatar';
+
 
 function Projetos() {
     function voltar() {
         window.history.back();
     }
     const actions = [
-        { icon: <Link to="/ProjetosConcluidos"><BsCheckSquare/></Link>, name: 'Concluido' },
+        { icon: <Link to="/ProjetosConcluidos"><BsCheckSquare /></Link>, name: 'Concluido' },
         { icon: <Link to="/Emplanejamento"><BsAlarm /></Link>, name: 'Em planejamento' },
-        { icon: <Link to="/Emdesenvolvimento"><BsHourglassSplit/></Link>, name: 'Em desenvovimento' },
-       
+        { icon: <Link to="/Emdesenvolvimento"><BsHourglassSplit /></Link>, name: 'Em desenvovimento' },
+
     ];
 
     const [post, setpost] = useState([])
@@ -49,23 +51,6 @@ function Projetos() {
             <div id="informativo">
                 <h1 id="titlepessoa" style={{ color: '#fff', marginLeft: '5%', marginTop: '4%' }}>Cadastro de Projetos</h1>
 
-                <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
-                    <SpeedDial
-                        ariaLabel="SpeedDial basic example"
-                        sx={{ position: 'absolute', bottom: 16, right: 16 }}
-                        icon={<SpeedDialIcon />}
-                    >
-                        {actions.map((action) => (
-                            <SpeedDialAction
-                                key={action.name}
-                                icon={action.icon}
-                                tooltipTitle={action.name}
-                            />
-                        ))}
-                    </SpeedDial>
-                </Box>
-
-
                 <Link to="/AddProjeto">
                     <button id="adicionar">Adicionar Projeto</button>
                 </Link>
@@ -82,21 +67,27 @@ function Projetos() {
 
                         <>
 
-   
-                                <div style={{ width: '18rem', height: '9rem', marginRight: '2%', background: '#21222D', borderRadius: '2rem' }} key={key}>
-                                    <div id="header" style={{ color: '#A9DFD8' }}>
-                                        <div className="poststatus">{post.status}</div>
 
-                                        <FiMoreVertical style={{ color: '#CCCCCC', display: 'flex', justifyContent: 'flex-end', marginLeft: '95%', marginTop: '-1%' }} />
-
+                            <div id="projetocard" style={{ background: '#171821' }}>
+                                <div style={{ width: '25rem', height: '15rem', marginRight: '2%', background: '#21222D', borderRadius: '0.7rem', padding: '3%' }} key={key}>
+                                    <div id="header" style={{ color: '#fff' }}>
+                                        {post.nome_projeto}
+                                        <div><FiMoreVertical style={{ color: '#CCCCCC', display: 'flex', justifyContent: 'flex-end', marginLeft: '95%', marginTop: '-1%' }} /></div>
                                     </div>
 
-                                    <Card.Body>
-                                        <h4 style={{ color: '#fff' }}>{post.nome_projeto}</h4>
-                                    </Card.Body>
-                                </div>
-                       
+                                    <div id="status">{post.status}</div>
 
+                                    <div id="descricao">
+                                        <p id="titulodesc">Descrição</p>
+                                        <p id="corpodesc">It is a long established fact that a reader will be distracted by the readable{post.descricao_projeto}</p>
+                                    </div>
+
+                                    <div id="membros">
+                                        <p>Membros</p>
+                                        <Avatar sx={{ fontSize: '0.5rem', width: '6%', height: '15px' }}>N</Avatar>
+                                    </div>
+                                </div>
+                            </div>
 
                         </>
 
@@ -105,10 +96,29 @@ function Projetos() {
 
                 })}
 
-
+                <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1, !impotant }}>
+                    <SpeedDial
+                        ariaLabel="SpeedDial basic example"
+                        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+                        icon={<SpeedDialIcon />}
+                    >
+                        {actions.map((action) => (
+                            <SpeedDialAction
+                                key={action.name}
+                                icon={action.icon}
+                                tooltipTitle={action.name}
+                            />
+                        ))}
+                    </SpeedDial>
+                </Box>
 
             </div>
+
+
+
         </>
+
+
     );
 
 }
