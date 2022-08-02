63459273b0b30f8e337f9005fdc013eb18597af3
IaraFV <iaraferrevit2@gmail.com>
2022-08-02 15:11:46 -0300

    icone letra funciona

--- src/components/Pessoas/Inspecionar/Inspecionar.js
+++ src/components/Pessoas/Inspecionar/Inspecionar.js
@@ -3,12 +3,14 @@ import './inspecionar.css'
 import { useNavigate, useParams } from 'react-router-dom'
 import axios from "axios";
 import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 import { Link } from 'react-router-dom';
 import { BsFillFileEarmarkFill } from "react-icons/bs";
 import CheckIcon from '@mui/icons-material/Check';
 import { AiOutlineStar } from "react-icons/ai";
+import { string } from "yup";
 
-    
+
 
 function Inspecionar() {
 
@@ -16,13 +18,13 @@ function Inspecionar() {
     const [posts, setPosts] = useState([])
     const { id_pessoa } = useParams()
 
-    
+
 
     useEffect(() => {
         axios.get(`https://sistema-aprendizes-brisanet-go.herokuapp.com/pessoas/${id_pessoa}`)
             .then((response) => {
                 setPosts(response.data)
-                
+
             })
             .catch(() => {
                 console.log("deu errado")
@@ -30,32 +32,37 @@ function Inspecionar() {
     }, []
     )
 
-//var nome = posts.nome_pessoa;
-//<Avatar  sx={{ width: '14rem', height: '14rem', fontSize: '8rem' }} aria-label="recipe">{nome.charAt(0)}</Avatar>
+    const nome = posts.nome_pessoa;
+    //<Avatar  sx={{ width: '14rem', height: '14rem', fontSize: '8rem' }} aria-label="recipe">{nome.charAt(0)}</Avatar>
 
-    
+
     function deletePost(id_pessoa) {
         axios.delete(`https://sistema-aprendizes-brisanet-go.herokuapp.com/pessoas/${id_pessoa}`)
         setPosts(posts.filter(post => post.id_pessoa !== id_pessoa))
     }
-    
+
+    function stringAvatar(name) {
+        return {
+            sx: {
+                //bgcolor: stringToColor(name),
+                width: '14rem', height: '14rem', fontSize: '8rem'
+            },
+            children: `${name.split(' ')[0][0]}`,
+        };
+    }
+
 
     return (
         <>
-       
-         
-           
-
             <div id="geral-card-inspecionar">
                 <div id="card-inspecionar">
-                <div id="card-header">
-
-                <Avatar  sx={{ width: '14rem', height: '14rem', fontSize: '8rem' }} aria-label="recipe">a</Avatar>
-
+                    <div id="card-header">
+                        <Avatar {...stringAvatar(`${nome}`)} />
 
 
-                </div>
-                   
+
+                    </div>
+
                     <div >
                         <h1>{posts.nome_pessoa}</h1>
                     </div>
@@ -92,7 +99,7 @@ function Inspecionar() {
                             </Link>
                         </div>
                         <div className="btn-excluir">
-                            <button onClick={() => deletePost(posts.id_pessoa) } aria-label="share" type="submit" to='/pessoas'>Deletar</button>
+                            <button onClick={() => deletePost(posts.id_pessoa)} aria-label="share" type="submit" to='/pessoas'>Deletar</button>
                         </div>
                     </div>
                 </div>
@@ -104,14 +111,14 @@ function Inspecionar() {
                         <div className="line-insp-dois"></div>
                     </div>
 
-                    
-                    <div id="cards-insp-tasks">
-                    </div>
-                    <div id="file" ><BsFillFileEarmarkFill/></div>
 
                     <div id="cards-insp-tasks">
                     </div>
-                    <div id="file" ><BsFillFileEarmarkFill/></div>
+                    <div id="file" ><BsFillFileEarmarkFill /></div>
+
+                    <div id="cards-insp-tasks">
+                    </div>
+                    <div id="file" ><BsFillFileEarmarkFill /></div>
                 </div>
             </div>
         </>
