12a5220c6e516edda162119ff148252bdfaae020
iara <iaraferrevit2@gmail.com>
2022-08-03 11:19:11 -0300

    projetos

--- src/components/Projetos/Concluidos/ProjetosConcluidos.js
+++ src/components/Projetos/Concluidos/ProjetosConcluidos.js
@@ -4,16 +4,8 @@ import Card from 'react-bootstrap/Card';
 import { useEffect, useState } from "react";
 import './Projetos.css';
 import { Link } from 'react-router-dom';
-import { FiMoreVertical } from "react-icons/fi";
-import Box from '@mui/material/Box';
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
-import { BsAlarm } from "react-icons/bs";
-import { BsCheckSquare } from "react-icons/bs";
-import { BsHourglassSplit } from "react-icons/bs";
+import { BsFillFlagFill } from "react-icons/bs";
 import Avatar from '@mui/material/Avatar';
-import { FiSearch } from "react-icons/fi";
 
 function Projetos() {
 
@@ -23,13 +15,8 @@ function Projetos() {
     function voltar() {
         window.history.back();
     }
-    const actions = [
-        { icon: <Link to="/ProjetosConcluidos"><BsCheckSquare /></Link>, name: 'Concluido' },
-        { icon: <Link to="/Emplanejamento"><BsAlarm /></Link>, name: 'Em planejamento' },
-        { icon: <Link to="/Emdesenvolvimento"><BsHourglassSplit /></Link>, name: 'Em desenvovimento' },
-
-    ];
-
+    
+    //get projetos
     const [post, setpost] = useState([])
     useEffect(() => {
         axios.get('https://sistema-aprendizes-brisanet-go.herokuapp.com/projetos/')
@@ -40,13 +27,16 @@ function Projetos() {
             })
     }, [])
 
+
+    //filter status
     const meuNovopost = post.filter((valorAtual) => {
         return valorAtual.status.includes("Em planejamento")
     })
+    console.log(meuNovopost);
 
-    console.log(meuNovopost)
-    const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
+
+    //filter pesquisa
     const handlechange = ({ target }) => {
         if (!target.value) {
             setPosts(initialPosts)
@@ -57,6 +47,8 @@ function Projetos() {
 
         setPosts(filter);
     }
+
+    
 
     return (
 
@@ -73,31 +65,37 @@ function Projetos() {
 
             </div>
 
-
-
             <div className="geralprojetoc">
-
                 {post.map((post, key) => {
                     return (
 
                         <>
-                            <div id="projetocard" style={{ border: 'none' }}>
-                                <div style={{ width: '23rem', height: '12rem', marginRight: '2%', background: '#21222D', borderRadius: '0.7rem', padding: '3%' }} key={key}>
-                                    <div id="header" style={{ color: '#fff' }}>
-                                        {post.nome_projeto}
-                                        <div><FiMoreVertical style={{ color: '#CCCCCC', display: 'flex', justifyContent: 'flex-end', marginLeft: '95%', marginTop: '-1%' }} /></div>
+                            <div id="projetocard" style={{ border: 'none', padding: '1%' }}>
+                                <div style={{ width: '23rem', height: '14rem', marginRight: '2%', background: '#21222D', borderRadius: '0.7rem', padding: '3%' }} key={key}>
+
+                                    <div id="status">
+                                        {post.status}
                                     </div>
 
-                                    <div id="status">{post.status}</div>
+
+                                    <div id="nome-projeto">
+                                        {post.nome_projeto}
+                                    </div>
+
+                                    <div id="data-inicio">
+                                        <div><BsFillFlagFill/></div>
+                                        {post.data_inicio}
+                                    </div>
+
 
                                     <div id="descricao">
                                         <p id="titulodesc">Descrição</p>
                                         <p id="corpodesc">It is a long established fact that a reader will be distracted by the readable{post.descricao_projeto}</p>
                                     </div>
 
-                                    <div id="membros">
-                                        <p>Membros</p>
-                                        <Avatar sx={{ fontSize: '0.5rem', width: '6%', height: '15px' }}>N</Avatar>
+                                    <div id="container-membros">
+                                        <p id="membros">Membros</p>
+                                        <Avatar  id="avatar-membros-equipes-projetos" sx={{ fontSize: '0.8rem', width: '30%', height: '1.2rem', marginTop: '-18%' }}>N</Avatar>
                                     </div>
 
                                 </div>
