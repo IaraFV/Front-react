101ddfbf9f1e4826885e68bc5e82473b8099318a
iara <iaraferrevit2@gmail.com>
2022-08-05 09:20:12 -0300

    drag and drop funciona

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -6,6 +6,14 @@ import Card from 'react-bootstrap/Card';
 
 function Task() {
 
+
+    function log(message) {
+        console.log('> ' + message)
+    }
+
+    const cards = document.querySelectorAll('.card')
+    const dropzones = document.querySelectorAll('.dropzone')
+
     const [posts, setPosts] = useState([])
 
     useEffect(() => {
@@ -36,21 +44,69 @@ function Task() {
 
     var Sta = stats.filter(states => states.status === 'Concluído');
 
+    /** our cards */
+    cards.forEach(card => {
+        card.addEventListener('dragstart', dragstart)
+        card.addEventListener('drag', drag)
+        card.addEventListener('dragend', dragend)
+    })
 
-    function allowDrop(ev) {
-        ev.preventDefault();
-      }
-      
-      function drag(ev) {
-        ev.dataTransfer.setData("t", ev.target.id);
-      }
-      
-      function drop(ev) {
-        ev.preventDefault();
-        var data = ev.dataTransfer.getData("t");
-        ev.target.appendChild(document.getElementById(data));
-      }
-      
+    function dragstart() {
+        // log('CARD: Start dragging ')
+        dropzones.forEach(dropzone => dropzone.classList.add('highlight'))
+
+        // this = card
+        this.classList.add('is-dragging')
+    }
+
+    function drag() {
+        log('CARD: Is dragging ')
+    }
+
+    function dragend() {
+        // log('CARD: Stop drag! ')
+        dropzones.forEach(dropzone => dropzone.classList.remove('highlight'))
+
+        // this = card
+        this.classList.remove('is-dragging')
+    }
+
+    /** place where we will drop cards */
+    dropzones.forEach(dropzone => {
+        dropzone.addEventListener('dragenter', dragenter)
+        dropzone.addEventListener('dragover', dragover)
+        dropzone.addEventListener('dragleave', dragleave)
+        dropzone.addEventListener('drop', drop)
+    })
+
+    function dragenter() {
+        // log('DROPZONE: Enter in zone ')
+    }
+
+    function dragover() {
+        // this = dropzone
+        this.classList.add('over')
+
+        // get dragging card
+        const cardBeingDragged = document.querySelector('.is-dragging')
+
+        // this = dropzone
+        this.appendChild(cardBeingDragged)
+    }
+
+    function dragleave() {
+        // log('DROPZONE: Leave ')
+        // this = dropzone
+        this.classList.remove('over')
+
+    }
+
+    function drop() {
+        // log('DROPZONE: dropped ')
+        this.classList.remove('over')
+    }
+
+
     return (
         <div>
             <div className="cabecalho">
@@ -58,22 +114,37 @@ function Task() {
                 <input type="text" className="input" placeholder="Ex: hello"></input>
             </div>
 
+
+
+
+
+
             <div className="d-flex">
                 <div className="col-3">
 
 
                 </div>
 
+
+
+
+
+
+
                 <div className="col-9 d-flex justify-content-between" style={{ height: "800px" }}>
-                    <div className="col-3 d-flex flex-column align-items-center" style={{ height: "745px" }} id="div1" ondrop="drop(event)" ondragover="allowDrop(event)">
+
+
+                    <div className="col-3 d-flex flex-column align-items-center" style={{ height: "745px" }} class="board">
                         <h4 className="text-center mt-2">
                             A fazer
                         </h4>
-                        { 
-                          and.map((post, key) => {
+
+
+                        {
+                            and.map((post, key) => {
                                 return (
-                                    <div>
-                                        <Card style={{ width: '18rem' }} id="drag1" draggable="true" ondragstart="drag(event)">
+                                    <div class="dropzone">
+                                        <Card style={{ width: '18rem' }} class="card" draggable="true">
                                             <Card.Body>
                                                 <Card.Title style={{ color: 'black' }} key={key}>{post.descricao_task}</Card.Title>
                                                 <Card.Text>{post.status}
@@ -85,16 +156,21 @@ function Task() {
                             })
                         }
                     </div>
-                    <div className="col-3 d-flex flex-column align-items-center" style={{ height: "745px" }}id="div1" ondrop="drop(event)" ondragover="allowDrop(event)" >
+
+
+
+
+
+                    <div className="col-3 d-flex flex-column align-items-center" style={{ height: "745px" }} class="board" >
                         <h4 className="text-center mt-2">
                             Em desenvolvimento
                         </h4>
-                        { 
-                          fi.map((post, key) => {
+                        {
+                            fi.map((post, key) => {
                                 return (
-                                    <div >
-                                        <Card style={{ width: '18rem' }} id="drag1" draggable="true" ondragstart="drag(event)">
-                                            <Card.Body>
+                                    <div class="dropzone">
+                                        <Card style={{ width: '18rem' }} draggable="true">
+                                            <Card.Body class="dropzone">
                                                 <Card.Title style={{ color: 'black' }} key={key}>{post.descricao_task}</Card.Title>
                                                 <Card.Text>{post.status}
                                                 </Card.Text>
@@ -105,15 +181,15 @@ function Task() {
                             })
                         }
                     </div>
-                    <div className="col-3 d-flex flex-column align-items-center" style={{ height: "745px" }} id="div1" ondrop="drop(event)" ondragover="allowDrop(event)">
+                    <div className="col-3 d-flex flex-column align-items-center" style={{ height: "745px" }} class="board">
                         <h4 className="text-center mt-2">
                             Concluídos
                         </h4>
-                        { 
-                          Sta.map((post, key) => {
+                        {
+                            Sta.map((post, key) => {
                                 return (
-                                    <div>
-                                        <Card style={{ width: '18rem' }} id="drag1" draggable="true" ondragstart="drag(event)">
+                                    <div class="dropzone">
+                                        <Card style={{ width: '18rem' }} draggable="true">
                                             <Card.Body>
                                                 <Card.Title style={{ color: 'black' }} key={key}>{post.descricao_task}</Card.Title>
                                                 <Card.Text>{post.status}
