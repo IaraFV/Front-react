db60cbb249e0cbf23f63f259b11f42c62c233806
Lucas <lucasmartinsnara@gmail.com>
2022-08-11 09:04:36 -0300

    up

--- src/components/Pessoas/Inspecionar/Inspecionar.js
+++ src/components/Pessoas/Inspecionar/Inspecionar.js
@@ -3,14 +3,17 @@ import './inspecionar.css'
 import { useNavigate, useParams } from 'react-router-dom'
 import axios from "axios";
 import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
 import { Link } from 'react-router-dom';
 import { BsFillFileEarmarkFill } from "react-icons/bs";
 import CheckIcon from '@mui/icons-material/Check';
 import { AiOutlineStar } from "react-icons/ai";
-import { string } from "yup";
 import { BsArrowLeft } from "react-icons/bs";
-
+import { AiOutlinePlus } from "react-icons/ai";
+/** */
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardHeader from '@mui/material/CardHeader';
 
 function Inspecionar() {
 
@@ -18,6 +21,7 @@ function Inspecionar() {
 
     let navigate = useNavigate()
     const [posts, setPosts] = useState([])
+    const [Task, setTask] = useState([])
     const { id_pessoa } = useParams()
 
 
@@ -26,6 +30,18 @@ function Inspecionar() {
         axios.get(`https://sistema-aprendizes-brisanet-go.herokuapp.com/pessoas/${id_pessoa}`)
             .then((response) => {
                 setPosts(response.data)
+
+            })
+            .catch(() => {
+                console.log("deu errado")
+            })
+    }, []
+    )
+    useEffect(() => {
+        axios.get(`https://sistema-aprendizes-brisanet-go.herokuapp.com/tasks/`)
+            .then((response) => {
+                setTask(response.data)
+                console.log("deu certo boy")
 
             })
             .catch(() => {
@@ -52,24 +68,22 @@ function Inspecionar() {
             children: `${name.split(' ')[0][0]}`,
         };
     }
- 
+    const ArrGeral_task = Task;
+    const recebId_pessoa = posts.id_pessoa;
+    const idPessoaINT = parseInt(recebId_pessoa);
+    const filtra_task = ArrGeral_task.filter(task => task.pessoa_id === idPessoaINT);
+
+
     return (
         <>
-
-
             <div id="geral-card-inspecionar">
                 <div id="card-inspecionar">
-
-
                     <Link to='/Pessoas'>
                         <BsArrowLeft id="voltar-insp" />
                     </Link>
-
-
                     <div id="card-header">
                         <Avatar {...stringAvatar(`${nome}`)} />
                     </div>
-
                     <div id="h1-insp">
                         <h1>{posts.nome_pessoa}</h1>
                     </div>
@@ -81,7 +95,6 @@ function Inspecionar() {
                                 <p style={{ marginTop: '-20%', color: '#fff', width: '5rem' }}>Tasks feitas</p>
                             </div>
                         </div>
-
                         <div id="estatisticadois">
 
                             <button onClick={() => setFavoritar(!favoritar)} className="star">
@@ -89,14 +102,10 @@ function Inspecionar() {
                             </button>
 
                             <p style={{ color: "rgba(186, 186, 186, 0.87)", fontSize: "1.2rem" }}>
-                                
-                               
                                 {favoritar ? 0 : 1}
                             </p>
-
                         </div>
                     </div>
-
                     <div id="detalhes">
                         <div id="cabecariodetalhes">
                             <h4>Detalhes</h4>
@@ -108,7 +117,6 @@ function Inspecionar() {
                             <p>Data de contratação: {posts.data_contratacao}</p>
                         </div>
                     </div>
-
                     <div id="botoes-insp">
                         <div className="btn-editar">
                             <Link to={{ pathname: `/Edit/${posts.id_pessoa}` }}>
@@ -120,22 +128,37 @@ function Inspecionar() {
                         </div>
                     </div>
                 </div>
-
                 <div id="card-inspdois">
-
                     <div id="card-header-insp">
-                        <h2>Task's</h2>
+                        <div id="btn-addTask-pessoaInsp">
+                            <h2>Task's</h2>
+                            <Link to="/PostT">
+                                <AiOutlinePlus className="btn-adicionartesk"></AiOutlinePlus>
+                            </Link>
+                        </div>
                         <div className="line-insp-dois"></div>
                     </div>
-
-
-                    <div id="cards-insp-tasks">
+                    <div id="lucas">
+                        {filtra_task.map((tasks) => {
+                            return (
+                                <div id="try">
+                                    <Card sx={{ minWidth: 175 }} id="cardtask">
+                                        <CardContent id="test">
+                                            <CardHeader
+                                                avatar={
+                                                    <Avatar  aria-label="recipe">
+                                                        <BsFillFileEarmarkFill id="cor"/>
+                                                    </Avatar>
+                                                }></CardHeader>
+                                            <Typography variant="h5" id="titulo" component="div">
+                                                {tasks.descricao_task}
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </div>
+                            )
+                        })}
                     </div>
-                    <div id="file" ><BsFillFileEarmarkFill /></div>
-
-                    <div id="cards-insp-tasks">
-                    </div>
-                    <div id="file" ><BsFillFileEarmarkFill /></div>
                 </div>
             </div>
         </>
