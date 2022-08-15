4957393b58026b95a025aa3be24cd18664aabb87
iara <iaraferrevit2@gmail.com>
2022-08-15 11:11:19 -0300

    up

--- src/components/Graficos/graf2.js
+++ src/components/Graficos/graf2.js
@@ -1,8 +1,7 @@
 
-import React, { PureComponent } from 'react';
-import {   Cell, } from 'recharts';
-import { Chart } from "react-google-charts";
+import React, { PureComponent, useState, useEffect } from 'react';
 import { PieChart, Pie, Sector, ResponsiveContainer } from "recharts";
+import axios from "axios";
 import {
   BarChart,
   Bar,
@@ -13,6 +12,9 @@ import {
   Legend
 } from "recharts";
 
+
+
+
 const data = [
   {
     name: "Page A",
@@ -20,46 +22,15 @@ const data = [
     pv: 2400,
     amt: 2400
   },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
+  
 ];
 
+
 export class Graf2 extends PureComponent {
-  render() {
+
+
+render() {
+
   return (
     <BarChart
       width={500}
