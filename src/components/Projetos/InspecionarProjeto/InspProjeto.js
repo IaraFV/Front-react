e534f3caee737920ab2fa63d02396396a276e272
iara <iaraferrevit2@gmail.com>
2022-08-29 11:30:50 -0300

    insp projeto

--- src/components/Projetos/InspecionarProjeto/InspProjeto.js
+++ src/components/Projetos/InspecionarProjeto/InspProjeto.js
@@ -14,7 +14,8 @@ import Typography from '@mui/material/Typography';
 import NativeSelect from '@mui/material/NativeSelect';
 import FormControl from '@mui/material/FormControl';
 import { AiOutlineMore } from "react-icons/ai";
-
+import { IoEllipseSharp } from "react-icons/io5";
+import { FiPlus } from "react-icons/fi";
 function InspProjeto() {
 
     //variaves das requisições GET
@@ -193,13 +194,13 @@ function InspProjeto() {
                         filtFazer.map((projetos, key) => {
                             return (
                                 <div className="dropzone" style={{ border: '1px solid red' }} >
-                                    <Card  className='card-color' id="card-afazer">
+                                    <Card className='card-color' id="card-afazer">
                                         <div className="menu-dos-filtros-statusTask">
                                             <div className="menu-dos-filtros-statusTask">
                                                 <button onClick={() => handleOpen(projetos.id_task)} className='btn-muda-status'></button>
                                                 <Card.Text>{projetos.status}</Card.Text>
                                             </div>
-                                             
+
                                         </div>
                                         <Card.Body>
                                             <Card.Title style={{ color: 'black' }} key={key}>{projetos.descricao_task}</Card.Title>
@@ -326,21 +327,24 @@ function InspProjeto() {
                 <div className="col-12 d-flex justify-content-around" style={{ height: "720px", border: '1px solid red' }}>
                     <div className="col-3 d-flex flex-column align-items-center" id="BarraRolagem"  >
                         <div id="header-status-afazer">
+                            <IoEllipseSharp id="icon-redondo-status-afazer" />
                             A fazer
+                            <FiPlus id="icon-add-task" />
                         </div>
                         <VerificaAfazer />
                     </div>
 
                     <div className="col-3 d-flex flex-column align-items-center" id="BarraRolage" style={{ height: "745px" }} >
-                        <h4 className="text-center mt-2">
+                        <div id="header-status-desenv">
+                        <IoEllipseSharp id="icon-redondo-status-desenv" />
                             Em desenvolvimento
-                        </h4>
+                        </div>
                         <VerificaDesenvolvimento />
                     </div>
                     <div className="col-3 d-flex flex-column align-items-center" id="BarraRolag" style={{ height: "745px" }} >
-                        <h4 className="text-center mt-2">
+                        <div className="text-center mt-2">
                             Concluídos
-                        </h4>
+                        </div>
                         <VerificaConcluído />
                     </div>
                 </div>
