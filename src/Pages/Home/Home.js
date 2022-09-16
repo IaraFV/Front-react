4b004e535943b9c87387c731447760b735947fbb
iara <iaraferrevit2@gmail.com>
2022-09-16 09:33:56 -0300

    :lipstick: alterações em projeto home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -10,9 +10,7 @@ import {
 import { BsBarChartLine } from "react-icons/bs";
 import api from "../../services/api";
 import nenhumprojeto from "../../assets/NenhumProjeto/Group 1000004639.png";
-import { Empty } from "antd";
-
-import Card from "react-bootstrap/Card";
+import { CardP, CardTitle, CardBody,Status, CardText, OrgCard, CardTextData } from "./style";
 import { BsFlagFill } from "react-icons/bs";
 import imagemerro from "./img/itensNaoencontrados.png";
 import { AiOutlineArrowsAlt } from "react-icons/ai";
@@ -82,11 +80,11 @@ function Home() {
     (getstatus) => getstatus.status === "Concluído"
   );
   var receberProj = [];
-  for (var pega = 0; pega < 2; pega++) {
+  for (var pega = 0; pega < 3; pega++) {
     receberProj.push(recebeprojetos[pega]);
   }
 
-  console.log(receberProj)
+  console.log(receberProj);
 
   //contadores das variaveis
   const totalpessoas = pessoas.length;
@@ -94,45 +92,48 @@ function Home() {
   const totaltask = tasks.length;
   const totalequipes = equipes.length;
 
+
+  function formdata(data) {
+    let Data = new Date(data);
+    return Data.toLocaleDateString("pt-BR")
+  }
+
+
   //função de verificação de erro
   function Apresentaproj() {
     return (
       <>
-        {receberProj?.map((status) => {
-          return (
-            <>
-              <Card style={{ width: "35rem" }}>
-                <Card.Body>
-                  <Card.Title>
-                    {projeto.nome_projeto}
-                    <div>
-                      <Link
-                        to={{ pathname: `/InspProjeto/${projeto.id_projeto}` }}
-                      >
-                        <AiOutlineArrowsAlt id="more-button-planejamento" />
-                      </Link>
-                    </div>
-                  </Card.Title>
-                  <Card.Text id="status">
-                    <span>{projeto.status}</span>
-                  </Card.Text>
-                  <Card.Text id="bandeira-data">
-                    <div>
-                      <BsFlagFill />
-                      {projeto.data_inicio}
-                    </div>
-                  </Card.Text>
-                  <Card.Text>
-                    <div className="titulo-descricao-projeto">Descrição</div>
-                    <div id="corpo-descricao-projeto">
-                      {projeto.descricao_projeto}
-                    </div>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </>
-          );
-        })}
+        <OrgCard>
+          {receberProj.map((receberProj, key) => {
+            return (
+              <>
+
+                <CardP>
+                  <CardBody>
+                    <CardTitle>
+                      {receberProj?.nome_projeto}
+                    </CardTitle>
+                    <Status>
+                      <span>{receberProj?.status}</span>
+                    </Status>
+                    <CardTextData>
+                      <div>
+                        <BsFlagFill />
+                        {formdata(receberProj?.data_inicio)}
+                      </div>
+                    </CardTextData>
+                    <CardText>
+                      <div className="titulo-descricao-projeto">Descrição</div>
+                      <div id="corpo-descricao-projeto">
+                        {receberProj?.descricao_projeto}
+                      </div>
+                    </CardText>
+                  </CardBody>
+                </CardP>
+              </>
+            );
+          })}
+        </OrgCard>
       </>
     );
   }
@@ -221,132 +222,3 @@ function Home() {
   );
 }
 export default Home;
-
-/*
-
- <div className="container">
-                <div className="row">
-                    <div className="col-12 col-sm-12">
-                        <div className="row justify-content-evelyn" id="caixa-geral-home-um">
-                            <div className="container" id="caixa-geral-projetos-home">
-                                <div className="row" >
-                                    <div className="card" id="render-projetos-conc-home">
-                                        <div className="card-body">
-                                            <h5 id="header-card-projeto-pagina-home" className="card-title">Ultimos projetos Concluidos</h5>
-                                            <p className="card-text"></p>
-                                        </div>
-                                        <ul className="list-group list-group-flush" id="ul-projeto">
-                                            <Apresentaproj/>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col-9" id="container-geral-dois-home">
-                                <div className="container">
-                                    <div className="row" id="col1-quant">
-                                        <div className="col-6 col-sm-4" id="card-quant-pessoa">
-                                            <div id="cont-quant-pessoa">
-                                                <div >
-                                                    <AiOutlineUser id="icon-quant-pessoa-home" />
-                                                </div>
-                                                <div id="total-pessoas-page-home">{totalpessoas}</div>
-                                                <div className="colorfff">Total de pessoas adiconadas</div>
-                                            </div>
-                                        </div>
-
-                                        <div className="col-6 col-sm-4" id="card-quant-pessoa">
-                                            <div id="cont-quant-projetos">
-                                                <div >
-                                                    <AiOutlineSolution id="icon-quant-projetos-home" />
-                                                </div>
-                                                <div id="total-projetos-page-home">{totalprojetos}</div>
-                                                <div className="colorfff">Total de projetos adiconadas</div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="row" id="col2-quant">
-                                        <div className="col-6 col-sm-4" id="card-quant-equipes">
-                                            <div id="cont-quant-equipes">
-                                                <div >
-                                                    <AiOutlineTeam id="icon-quant-equipes-home" />
-                                                </div>
-                                                <div id="total-equipes-page-home">{totalequipes}</div>
-                                                <div className="colorfff">Total de equipes adiconadas</div>
-                                            </div>
-
-                                        </div>
-
-                                        <div className="col-6 col-sm-4" id="card-quant-de-tasks">
-                                            <div id="container-quantidade-task">
-                                                <div >
-                                                    <BsBarChartLine id="icon-quant-task-page-home" />
-                                                </div>
-                                                <div id="total-de-tasks-pagina-home-h">{totaltask}</div>
-                                                <div className="colorfff">Total de tarefas adiconadas</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="w-100 d-none d-md-block"></div>
-                    <div className="row">
-                        <div className="col-12 col-sm-12">
-                            <div className="row justify-content-between" id="caixa-dois-home">
-                                <div className="col-2">
-                                    <div className="col-6 col-sm-4">
-                                        <div className="row" id="geral-graficoum">
-                                            <div className="col-6 col-sm-4">
-                                                <Grafico2 pessoas={pessoas} />
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="col-2">
-                                    <div className="col-6 col-sm-4" id="sla">
-                                        <div className="card" id="render-pessoas-home" >
-                                            <div className="card-body">
-                                                <h5 className="card-title" id="titulo-card-usuario-home">
-                                                    Usuarios
-                                                    <Link id="link-pessoa-page-pessoa" to={"/Pessoas"}>
-                                                        <button id="btn-ver-usuario-projeto">Ver Usuarios</button>
-                                                    </Link>
-                                                </h5>
-                                                <h6 className="card-subtitle mb-2 text-muted"></h6>
-                                                <ul className="list-group list-group-flush" id="ul-pessoa">
-                                                    {
-                                                        nomepeople.map((nome) => {
-                                                            return (
-                                                                <>
-                                                                    <li className="list-group-item" id="li-pessoa">{nome}</li>
-                                                                </>
-                                                            );
-                                                        })
-                                                    }
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-*/
-
-/*div id="caixa-geral-projetos-home"><div className="card" id="render-projetos-conc-home">
-            <div className="card-body">
-              <h5 id="header-card-projeto-pagina-home" className="card-title">Ultimos projetos Concluidos</h5>
-              <p className="card-text"></p>
-            </div>
-            <ul className="list-group list-group-flush" id="ul-projeto">
-              <Apresentaproj/>
-            </ul>
-          </div></div> */
---
+++ src/Pages/Home/style.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const OrgCard = styled.div`
+    width: 66rem;
+    display: flex;
+    justify-content: space-between;
+    
+`;
+
+export const CardP = styled.div`
+    width: 21rem;
+    border-radius: 10px;
+    background: #21222D;
+    padding: 0.5rem;
+`;
+
+export const CardBody = styled.div`
+    
+`;
+export const CardTitle= styled.div`
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 1.3rem;
+    line-height: 27px;
+    color: #FFFFFF;
+`;
+
+export const Status = styled.div`
+    font-family: 'Roboto';
+    font-style: normal;
+    font-size: 1rem;
+    line-height: 15px;
+    margin-top: 2%;
+    color: rgba(0, 219, 153, 0.9); 
+ `;
+
+ export const CardTextData = styled.div`
+    font-style: normal;
+    font-weight: 500;
+    font-size: 0.8rem;
+    line-height: 15px;
+    color: #717986;
+    margin-top: 4%;
+`;
+export const CardText = styled.div`
+    margin-top: 4%;
+`;
